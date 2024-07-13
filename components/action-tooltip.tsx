76e590185ba00233eefe586ae45890024c7df387
Jaxon <127579285+JaxonDevs@users.noreply.github.com>
2024-07-13 15:12:44 +1000

    errors  :(

---
+++ components/action-tooltip.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip";
+
+interface ActionTooltipProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
+}
+
+export function ActionTooltip({
+  children,
+  label,
+  align,
+  side
+}: ActionTooltipProps) {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={50}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent side={side} align={align}>
+          <p className="font-semibold text-sm capitalize">
+            {label.toLowerCase()}
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
